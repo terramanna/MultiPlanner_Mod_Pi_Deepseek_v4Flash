fa644aea9dcfcc9cd04c9884f7fd82d073a1f9e6
Terra <terramanna@users.noreply.github.com>
2026-09-19 22:30:30 +0200

    fix: restore Leaflet as default renderer, Cesium as opt-in

--- apps/web/src/bootstrap.js
+++ apps/web/src/bootstrap.js
@@ -1,9 +1,9 @@
 const renderer = new URLSearchParams(window.location.search).get("renderer");
 
-if (renderer === "cesium") {
+if (renderer === "maplibre") {
+  await import("./maplibre-prototype.js");
+} else if (renderer === "cesium") {
   await import("./main.js");
-} else if (renderer === "leaflet" || renderer === "leaflet-nrw") {
-  await import("./leaflet-prototype.js");
 } else {
-  await import("./main.js");
+  await import("./leaflet-prototype.js");
 }
